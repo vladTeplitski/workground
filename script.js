98a9f6213b6e1a3e0c58f9764d5dfd88073ef093
Vlad <vl4d89@gmail.com>
2018-11-08 21:50:42 +0200

    Datasets

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 //test url: https://irwebsites.co.il/workground/index.html
 
 //Globals
-var json_obj;
+var json_obj = null;
+var interval;
 
 function initialize(){
     
@@ -29,45 +30,77 @@ function callPhp(){
     xmlhttp.open("GET", "getData.php", true);
     xmlhttp.send(); 
 
-    createChart();
-    
+}
+
+interval = setInterval("getResult()", 200);
+function getResult()
+{
+    // once we get a result, turn interval off. 
+  if(json_obj != null)
+  {
+    interval = clearInterval(interval);
+    //create json from the data
+    var newobj = JSON.parse(json_obj);
+    createChart(newobj);
+  }
 }
 
 //Data builders
-function buildxData(){
-    //document.getElementById("debug3").innerHTML = "Into buildxData";
-    //var data = callPhp();
-    //document.getElementById("debug4").innerHTML = data.date1;
-    //var arr = [];
-    //arr.push(data.date1);
-    //arr.push(data.date2);
-    //arr.push(data.date3);
-    //arr.push(data.date4);
-    //arr.push(data.date5);
+function buildxData(data){
+    var arr = [];
+    var i;
+    document.getElementById("debug5").innerHTML = "Into buildxData";
+    
+    arr.push(data.date1);
+    arr.push(data.date2);
+    arr.push(data.date3);
+    arr.push(data.date4);
+    arr.push(data.date5);
 
-    //var finalData = [];
-    //for(i=0;i<arr.length();i++){
-    //    if(arr[i]!="-"){
-    //        finalData.push(arr[i]);
-    //    }
-    //}
+    document.getElementById("debug6").innerHTML = arr;
+    var finalData = [];
+    for(i=0;i<arr.length;i++){
+        if(arr[i]!="-"){
+            finalData.push(arr[i]);
+        }
+    }
 
-    var data = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
-    document.getElementById("debug4").innerHTML = "Using json data";
-    document.getElementById("debug4").innerHTML = json_obj;
-    return data;
+    //var finalData = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
+    document.getElementById("debug7").innerHTML = finalData;
+    return finalData;
 }
 
-//function buildyData(){
-//    var data = callPhp()
-//    return data;
-//}
+
+function buildyData(data){
+  var arr = [];
+  var i;
+  document.getElementById("debug8").innerHTML = "Into buildyData";
+  
+  arr.push(data.vol1);
+  arr.push(data.vol2);
+  arr.push(data.vol3);
+  arr.push(data.vol4);
+  arr.push(data.vol5);
+
+  document.getElementById("debug9").innerHTML = arr;
+  var finalData = [];
+  for(i=0;i<arr.length;i++){
+      if(arr[i]!="-"){
+          finalData.push(arr[i]);
+      }
+  }
+
+  //var finalData = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
+  document.getElementById("debug10").innerHTML = finalData;
+  return finalData;
+}
 
 
 //Chart builder
-function createChart(){
+function createChart(dataset){
+  //document.getElementById("debug4").innerHTML = dataset;
         //x axis values
-        var x_data = buildxData();
+        var x_data = buildxData(dataset);
        //chart Object init
        data = {
         labels: x_data,
@@ -89,7 +122,7 @@ function createChart(){
             stacked: true,
             gridLines: {
               display: true,
-              color: "rgba(255,99,132,0.2)"
+              color: "#B8D7EC"
             }
           }],
           xAxes: [{
@@ -103,16 +136,16 @@ function createChart(){
       var graph01 = Object.create(graph);
       graph01.data = [200,1200,4052,7521,1025,2555,3654,4562,2478,1256];
       graph01.label = "Akko";
-      graph01.borderColor = "#CAD118";
+      graph01.borderColor = "#124784";
 
       var graph02 = Object.create(graph);
-      graph02.data = [40,20,1000,16,24,2000,74,4500,508,784];
-      //graph02.data = [40,20];
-      graph02.label = "Haifa";
-      graph02.borderColor = "#c45850";
+      //graph02.data = [40,20,1000,16,24,2000,74,4500,508,784];
+      graph02.data = [40,20,40,30,45];
+      graph02.label = "Weekly";
+      graph02.borderColor = "#128435";
 
       //add graphs
-      data.datasets.push(graph01);
+      //data.datasets.push(graph01);
       data.datasets.push(graph02);
       
       //Build the chart
